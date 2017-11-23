0b0adea854caab3f058701dcfabc13f699c781e7
Rikka <rikka@shizuku.moe>
2017-11-23 15:26:55 +0800

    feat: add LocalImageGetter

---
+++ support-utils/src/main/java/moe/shizuku/support/text/LocalImageGetter.java
@@ -0,0 +1,57 @@
+package moe.shizuku.support.text;
+
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by rikka on 2017/11/7.
+ */
+
+public class LocalImageGetter implements Html.ImageGetter {
+
+    private static final String TAG = "LocalImageGetter";
+
+    private static final String RAW_PREFIX = "file:///android_res/raw/";
+
+    private TextView mTextView;
+
+    public LocalImageGetter(TextView textView) {
+        mTextView = textView;
+    }
+
+    @Override
+    public Drawable getDrawable(String url) {
+        Drawable drawable = null;
+
+        try {
+            Resources resources = mTextView.getResources();
+            int maxWidth = mTextView.getWidth() - mTextView.getPaddingLeft() - mTextView.getPaddingRight();
+
+            if (url.startsWith(RAW_PREFIX)) {
+                String name = url.substring(RAW_PREFIX.length(), url.lastIndexOf('.'));
+                int res = resources.getIdentifier(name, "raw", mTextView.getContext().getPackageName());
+                drawable = new BitmapDrawable(resources, BitmapFactory.decodeStream(resources.openRawResource(res)));
+
+                int width = drawable.getIntrinsicWidth();
+                int height = drawable.getIntrinsicHeight();
+                if (width > maxWidth) {
+                    height *= (float) maxWidth / width;
+                    width = maxWidth;
+                }
+
+                drawable.setBounds(0, 0, width, height);
+            } else {
+                Log.w(TAG, "unsupported url: " + url);
+            }
+        } catch (Throwable e) {
+            Log.w(TAG, "can't load image: " + url, e);
+        }
+
+        return drawable;
+    }
+}
