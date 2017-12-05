ef832deb0a8915892f369a87b6dd3475a40faf50
Rikka <rikka@shizuku.moe>
2017-12-05 20:55:10 +0800

    feat: update HtmlUtils

--- support-utils/src/main/java/moe/shizuku/support/utils/HtmlUtils.java
+++ support-utils/src/main/java/moe/shizuku/support/utils/HtmlUtils.java
@@ -10,12 +10,38 @@ import android.text.Spanned;
 
 public class HtmlUtils {
 
+    private static final int FROM_HTML_MODE_LEGACY;
+
+    static {
+        if (Build.VERSION.SDK_INT >= 24) {
+            FROM_HTML_MODE_LEGACY = Html.FROM_HTML_MODE_LEGACY;
+        } else {
+            FROM_HTML_MODE_LEGACY = 0;
+        }
+    }
+
     public static Spanned fromHtml(String source) {
+        return fromHtml(source, FROM_HTML_MODE_LEGACY, null, null);
+    }
+
+    public static Spanned fromHtml(String source, Html.ImageGetter imageGetter) {
+        return fromHtml(source, FROM_HTML_MODE_LEGACY, imageGetter, null);
+    }
+
+    public static Spanned fromHtml(String source, Html.TagHandler tagHandler) {
+        return fromHtml(source, FROM_HTML_MODE_LEGACY, null, tagHandler);
+    }
+
+    public static Spanned fromHtml(String source, Html.ImageGetter imageGetter, Html.TagHandler tagHandler) {
+        return fromHtml(source, FROM_HTML_MODE_LEGACY, imageGetter, tagHandler);
+    }
+
+    public static Spanned fromHtml(String source, int flags, Html.ImageGetter imageGetter, Html.TagHandler tagHandler) {
         Spanned htmlDescription;
         if (Build.VERSION.SDK_INT >= 24) {
-            htmlDescription = Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY);
+            htmlDescription = Html.fromHtml(source, flags, imageGetter, tagHandler);
         } else {
-            htmlDescription = Html.fromHtml(source);
+            htmlDescription = Html.fromHtml(source, imageGetter, tagHandler);
         }
 
         String descriptionWithOutExtraSpace = htmlDescription.toString().trim();
