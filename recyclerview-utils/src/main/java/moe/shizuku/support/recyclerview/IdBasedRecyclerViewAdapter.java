9a9ee3046e4e7fcc8091a41f657e2a15752a7325
Fung Gwo <fython@163.com>
2018-09-25 12:31:46 +0800

    Add IdBasedRecyclerViewAdapter
    Signed-off-by: Fung Gwo <fython@163.com>

---
+++ recyclerview-utils/src/main/java/moe/shizuku/support/recyclerview/IdBasedRecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package moe.shizuku.support.recyclerview;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class IdBasedRecyclerViewAdapter extends BaseRecyclerViewAdapter<IndexCreatorPool> {
+
+    private final List<Long> mIds = new ArrayList<>();
+
+    public IdBasedRecyclerViewAdapter() {
+        super();
+    }
+
+    public IdBasedRecyclerViewAdapter(@NonNull List<Object> data) {
+        super(data);
+    }
+
+    public List<Long> getIds() {
+        return mIds;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return mIds.get(position);
+    }
+
+    public void clear() {
+        getCreatorPool().clear();
+        getItems().clear();
+        getIds().clear();
+    }
+
+    public void addItem(@NonNull BaseViewHolder.Creator creator, @Nullable Object object, long id) {
+        getCreatorPool().add(creator);
+        getItems().add(object);
+        getIds().add(id);
+    }
+
+    public void addItems(@NonNull BaseViewHolder.Creator creator, @NonNull List<Object> list, @NonNull List<Long> ids) {
+        for (int index = 0; index < list.size(); index++) {
+            addItem(creator, list.get(index), ids.get(index));
+        }
+    }
+
+    public void notifyItemChangeById(long targetId) {
+        for (int index = 0; index < getItemCount(); index++) {
+            if (getIds().get(index) == targetId) {
+                notifyItemChanged(index);
+            }
+        }
+    }
+
+    public void notifyItemChangeById(long targetId, Object payload) {
+        for (int index = 0; index < getItemCount(); index++) {
+            if (getIds().get(index) == targetId) {
+                notifyItemChanged(index, payload);
+            }
+        }
+    }
+
+    public void setFirstItemById(long targetId, Object object) {
+        for (int index = 0; index < getItemCount(); index++) {
+            if (getIds().get(index) == targetId) {
+                getItems().set(index, object);
+                return;
+            }
+        }
+        throw new NoSuchElementException("Cannot found any items belongs to id=" + targetId);
+    }
+
+    @Override
+    public IndexCreatorPool onCreateCreatorPool() {
+        return new IndexCreatorPool();
+    }
+
+}
