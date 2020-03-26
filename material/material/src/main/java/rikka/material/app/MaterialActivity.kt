bd5f99720570839559d43eaf56eb9d40836b4078
RikkaW <rikka@shizuku.moe>
2020-03-26 17:04:24 +0800

    no navigationBarDividerColor by default

--- material/material/src/main/java/rikka/material/app/MaterialActivity.kt
+++ material/material/src/main/java/rikka/material/app/MaterialActivity.kt
@@ -1,9 +1,14 @@
 package rikka.material.app
 
+import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.annotation.CallSuper
 import androidx.appcompat.app.MaterialViewInflater
 import androidx.appcompat.widget.Toolbar
+import rikka.core.res.resolveColor
+import rikka.material.R
 import rikka.material.internal.ThemedAppCompatActivity
 import rikka.material.widget.AppBarLayout
 
@@ -31,5 +36,23 @@ open class MaterialActivity : ThemedAppCompatActivity(), AppBarOwner, Translucen
     override fun setAppBar(appBarLayout: AppBarLayout, toolbar: Toolbar) {
         super.setSupportActionBar(toolbar)
         appBar = AppBar(supportActionBar!!, appBarLayout)
+    }
+
+    override fun shouldApplyTranslucentSystemBars(): Boolean {
+        return true
+    }
+
+    @CallSuper
+    override fun onApplyTranslucentSystemBars() {
+        val window = window
+        val theme = theme
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window?.decorView?.post {
+                if (window.decorView.rootWindowInsets?.systemWindowInsetBottom ?: 0 >= Resources.getSystem().displayMetrics.density * 40) {
+                    window.navigationBarDividerColor = theme.resolveColor(R.attr.navigationBarDividerColor)
+                }
+            }
+        }
     }
 }
