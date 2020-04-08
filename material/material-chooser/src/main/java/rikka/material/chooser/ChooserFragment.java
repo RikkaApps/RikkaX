2c11bba360e697e82ab44fde650204dc92035251
RikkaW <rikka@shizuku.moe>
2020-04-08 14:48:13 +0800

    use AppIconLoader in chooser

--- material/material-chooser/src/main/java/rikka/material/chooser/ChooserFragment.java
+++ material/material-chooser/src/main/java/rikka/material/chooser/ChooserFragment.java
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.zhanghai.android.appiconloader.AppIconLoader;
 import rikka.material.internal.ThemedAppCompatDialogFragment;
 
 public class ChooserFragment extends ThemedAppCompatDialogFragment {
@@ -137,6 +138,8 @@ public class ChooserFragment extends ThemedAppCompatDialogFragment {
         return chooserFragment;
     }
 
+    private AppIconLoader mAppIconLoader;
+
     private String mTitle;
     private Intent mTargetIntent;
     private List<ResolveInfo> mResolves;
@@ -151,6 +154,8 @@ public class ChooserFragment extends ThemedAppCompatDialogFragment {
         if (getArguments() == null) {
             throw new IllegalArgumentException("Arguments cannot be null or empty.");
         }
+
+        mAppIconLoader = new AppIconLoader(requireContext().getResources().getDimensionPixelSize(R.dimen.rd_chooser_icon_size), requireContext());
 
         mTitle = getArguments().getString(ARG_TITLE);
         mTargetIntent = getArguments().getParcelable(ARG_TARGET_INTENT);
@@ -228,5 +233,9 @@ public class ChooserFragment extends ThemedAppCompatDialogFragment {
             return mExtraTargetIntent.get(index);
         }
         return mTargetIntent;
+    }
+
+    protected AppIconLoader getAppIconLoader() {
+        return mAppIconLoader;
     }
 }
---
+++ material/material-chooser/src/main/java/rikka/material/chooser/ChooserItemAdapter.kt
@@ -0,0 +1,88 @@
+package rikka.material.chooser
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.ResolveInfo
+import android.graphics.Bitmap
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import rikka.recyclerview.BaseViewHolder
+import rikka.recyclerview.IdBasedRecyclerViewAdapter
+
+internal class ChooserItemAdapter(resolveInfo: List<ResolveInfo?>, private val parentFragment: ChooserFragment) : IdBasedRecyclerViewAdapter() {
+
+    private val creator = BaseViewHolder.Creator<ResolveInfo> { inflater, parent -> ChooserItemViewHolder(inflater.inflate(R.layout.chooser_grid_item, parent, false)) }
+
+    init {
+        for ((index, it) in resolveInfo.withIndex()) {
+            addItem(creator, it, index.toLong())
+        }
+    }
+
+    private inner class ChooserItemViewHolder internal constructor(itemView: View) : BaseViewHolder<ResolveInfo?>(itemView), View.OnClickListener {
+
+        private val icon: ImageView = itemView.findViewById(android.R.id.icon)
+        private val title: TextView = itemView.findViewById(android.R.id.text1)
+
+        init {
+            setIsRecyclable(false)
+        }
+
+        override fun onClick(v: View) {
+            val context = v.context
+            val intent = Intent(parentFragment.getTargetIntent(data))
+            intent.component = ComponentName(
+                    data!!.activityInfo.packageName,
+                    data!!.activityInfo.name)
+            if (context !is Activity) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            try {
+                context.startActivity(intent)
+            } catch (ignored: Throwable) {
+            }
+            parentFragment.requireActivity().finish()
+        }
+
+        override fun onBind() {
+            super.onBind()
+            if (data == null) {
+                icon.setImageDrawable(null)
+                title.text = null
+                itemView.setOnClickListener(null)
+                itemView.isClickable = false
+                itemView.isFocusable = false
+                return
+            }
+            itemView.setOnClickListener(this)
+            itemView.isFocusable = true
+            val pm = itemView.context.packageManager
+            GlobalScope.launch(Dispatchers.IO) {
+
+            }
+            parentFragment.lifecycleScope.launchWhenCreated {
+                var drawable: Bitmap? = null
+                var label: CharSequence? = null
+                try {
+                    withContext(Dispatchers.IO) {
+                        drawable = parentFragment.appIconLoader.loadIcon(data!!.activityInfo.applicationInfo, false, true)
+                        label = data!!.loadLabel(pm)
+                    }
+                } catch (e: Throwable) {
+
+                }
+                icon.setImageBitmap(drawable)
+                title.text = label
+            }
+        }
+    }
+
+}
+
