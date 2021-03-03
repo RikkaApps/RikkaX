9bc3cb7c8faef45f10388a3f911aeb812f8c015f
RikkaW <rikka@shizuku.moe>
2021-03-03 16:49:11 +0800

    Port SimpleMenuPreference to androidx

---
+++ preference/simplemenu-preference/src/main/java/rikka/preference/SimpleMenuPreference.java
@@ -0,0 +1,138 @@
+package rikka.preference;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceViewHolder;
+
+import rikka.preference.simplemenu.R;
+import rikka.preference.simplemenu.SimpleMenuPopupWindow;
+
+/**
+ * A version of {@link ListPreference} that use Simple Menus in Material Design 1 as drop down.
+ * <p>
+ * On pre-Lollipop, it will fallback to {@link ListPreference}.
+ */
+
+public class SimpleMenuPreference extends ListPreference {
+
+    private static boolean sLightFixEnabled = false;
+
+    public static boolean isLightFixEnabled() {
+        return sLightFixEnabled;
+    }
+
+    public static void setLightFixEnabled(boolean lightFixEnabled) {
+        sLightFixEnabled = lightFixEnabled;
+    }
+
+    private View mAnchor;
+    private View mItemView;
+    private SimpleMenuPopupWindow mPopupWindow;
+
+    public SimpleMenuPreference(Context context) {
+        this(context, null);
+    }
+
+    public SimpleMenuPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP ? R.attr.dialogPreferenceStyle : R.attr.simpleMenuPreferenceStyle);
+    }
+
+    public SimpleMenuPreference(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, R.style.Preference_SimpleMenuPreference);
+    }
+
+    public SimpleMenuPreference(Context context, AttributeSet attrs, int defStyleAttr,
+                                int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+
+        TypedArray a = context.obtainStyledAttributes(
+                attrs, R.styleable.SimpleMenuPreference, defStyleAttr, defStyleRes);
+
+        int popupStyle = a.getResourceId(R.styleable.SimpleMenuPreference_android_popupMenuStyle, R.style.Widget_Preference_SimpleMenuPreference_PopupMenu);
+        int popupTheme = a.getResourceId(R.styleable.SimpleMenuPreference_android_popupTheme, R.style.ThemeOverlay_Preference_SimpleMenuPreference_PopupMenu);
+        Context popupContext;
+        if (popupTheme != 0) {
+            popupContext = new ContextThemeWrapper(context, popupTheme);
+        } else {
+            popupContext = context;
+        }
+
+        mPopupWindow = new SimpleMenuPopupWindow(popupContext, attrs, R.styleable.SimpleMenuPreference_android_popupMenuStyle, popupStyle);
+        mPopupWindow.setOnItemClickListener(i -> {
+            String value = getEntryValues()[i].toString();
+            if (callChangeListener(value)) {
+                setValue(value);
+            }
+        });
+
+        a.recycle();
+    }
+
+    @Override
+    protected void onClick() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            super.onClick();
+            return;
+        }
+
+        if (getEntries() == null || getEntries().length == 0) {
+            return;
+        }
+
+        if (mPopupWindow == null) {
+            return;
+        }
+
+        mPopupWindow.setEntries(getEntries());
+        mPopupWindow.setSelectedIndex(findIndexOfValue(getValue()));
+
+        View container = (View) mItemView   // itemView
+                .getParent();               // -> list (RecyclerView)
+
+        mPopupWindow.show(mItemView, container, (int) mAnchor.getX());
+    }
+
+    @Override
+    public void setEntries(@NonNull CharSequence[] entries) {
+        super.setEntries(entries);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+
+        mPopupWindow.requestMeasure();
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(value);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder view) {
+        super.onBindViewHolder(view);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+
+        mItemView = view.itemView;
+        mAnchor = view.itemView.findViewById(android.R.id.empty);
+
+        if (mAnchor == null) {
+            throw new IllegalStateException("SimpleMenuPreference item layout must contain" +
+                    "a view id is android.R.id.empty to support iconSpaceReserved");
+        }
+    }
+}
---
+++ preference/simplemenu-preference/src/main/java/rikka/preference/simplemenu/PropertyHolder.java
@@ -0,0 +1,39 @@
+package rikka.preference.simplemenu;
+
+import android.graphics.Rect;
+import android.os.Build;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+
+/**
+ * Holder class holds background drawable and content view.
+ */
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+class PropertyHolder {
+
+    private final CustomBoundsDrawable mBackground;
+    private final View mContentView;
+
+    public PropertyHolder(CustomBoundsDrawable background, View contentView) {
+        mBackground = background;
+        mContentView = contentView;
+    }
+
+    private CustomBoundsDrawable getBackground() {
+        return mBackground;
+    }
+
+    private View getContentView() {
+        return mContentView;
+    }
+
+    public Rect getBounds() {
+        return getBackground().getBounds();
+    }
+
+    public void setBounds(Rect value) {
+        getBackground().setCustomBounds(value);
+        getContentView().invalidateOutline();
+    }
+}
